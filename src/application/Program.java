f5b5b10e6f20313910294ea4e1f483a07d55ae72
Guilherme-Justo <guilherme-justo@hotmail.com>
2021-06-21 22:03:48 -0300

    Acrescentada mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Bom dia!");
+		
 		System.out.println("Enter product data:");
 		System.out.print("Name: ");
 		String name = sc.next();
